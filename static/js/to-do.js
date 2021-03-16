834c7c8b3496ede6d9df5bf122216ddf1b9bc84d
dovi-k <dovylee@gmail.com>
2021-03-16 14:31:34 +0000

    Deleted unused images. Created Add to-do js function

--- static/js/to-do.js
+++ static/js/to-do.js
@@ -8,4 +8,23 @@ const input = document.getElementById("input");
 
 const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
-const LINE_THROUGH
+const LINE_THROUGH = "lineThrough";
+
+// Show today's date
+
+const options = {weekday: "long", month: "short", day: "numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+// add to-do function
+
+function addToDo(toDo){
+    const item = `
+                <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+                <p class="text">${toDo}</p>
+                <i class="fa fa-trash-o de" job="delete" id="0"></i>
+    `;
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, item);
+}
