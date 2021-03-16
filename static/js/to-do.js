df5a208693f621ac5ebf05431f85c1a358d9df92
dovi-k <dovylee@gmail.com>
2021-03-16 21:47:57 +0000

    Adding 'add item' functionality using enter key

--- static/js/to-do.js
+++ static/js/to-do.js
@@ -29,3 +29,14 @@ function addToDo(toDo){
     list.insertAdjacentHTML(position, item);
 }
 
+// add an item to the list using the enter key
+
+document.addEventListener("keyup", function(event){
+    if(event.keyCode == 13){
+        const toDo = input.value;
+        // if the input isn't empty
+        if(toDo){
+            addToDo(toDo);
+        }
+    }
+});
