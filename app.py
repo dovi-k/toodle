bd8923825b7d1f7d8feb6c5679dc93ea9e4c2a9b
dovi-k <dovylee@gmail.com>
2021-03-22 12:25:03 +0000

    Added Flash messages display

--- app.py
+++ app.py
@@ -37,6 +37,24 @@ def profile():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("signup"))
+
+        signup = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(signup)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("signup.html")
 
 
